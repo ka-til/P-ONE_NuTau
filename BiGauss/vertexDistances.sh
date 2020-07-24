bfcb0868b889a6e202efdcc777fa0ec0a0241211
ka-til <akatil@illume-sub-0.illume-sub.illume.svc.cluster.local>
2020-07-24 17:25:29 +0000

    Time difference between two vertices

---
+++ BiGauss/vertexDistances.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+date
+startsecond=$(date +%s)
+
+echo "Starting the NuGen job"
+echo "Argument line : " $@
+
+echo "Starting cvmfs "
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.0.1/setup.sh`
+echo "cvfms ran"
+
+i3env=/cvmfs/icecube.opensciencegrid.org/py3-v4.0.1/RHEL_7_x86_64/metaprojects/combo/stable/env-shell.sh
+echo "Will use i3 environment: " ${i3env}
+script=/home/users/akatil/P-ONE/git/PONE_NuTau/BiGauss/vertexDistances.py
+echo "Will use script: " $script
+
+$i3env python $script
+
+date
+endsecond=$(date +%s)
+echo "End second: " $endsecond
+echo "This job took : "`expr $endsecond - $startsecond`" s"
