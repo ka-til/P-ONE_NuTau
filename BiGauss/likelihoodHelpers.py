2ffe45c5e732ec832db55b12a44074e19626a402
ka-til <akatil@illume-sub-0.illume-sub.illume.svc.cluster.local>
2020-09-11 20:07:31 +0000

    BiGaussian, Douple peak, likelihood fit, likelihood ratio

---
+++ BiGauss/likelihoodHelpers.py
@@ -0,0 +1,79 @@
+'''
+Functions commonly used for likelihood analysis
+'''
+from icecube import icetray, dataio, dataclasses, simclasses, clsim
+from icecube.icetray import I3Units, OMKey, I3Frame
+from icecube.dataclasses import ModuleKey
+from os.path import expandvars
+import argparse
+import numpy as np
+from scipy import stats
+from scipy.optimize import minimize
+from scipy.stats.distributions import chi2
+import scipy
+
+'''
+
+Functions used
+
+'''
+
+def gaussian(x, pos, wid, amp):
+    y = amp*np.exp(-4*np.log(2)*((x-pos)/(wid))**2)
+    return y
+
+def biGauss(x, pos, wid, r, amp):
+    mask = x < pos
+
+    y_all = ([])
+    for i in range(0, len(mask)):
+
+        if mask[i] == True:
+            m = 1
+            nm = 0
+        else:
+            m = 0
+            nm = 1
+        if r != 0:
+            y1 = gaussian(x[i],pos,r*wid/(r+1),amp)*m
+            y2 = gaussian(x[i],pos,wid/(r+1),amp)*nm
+            y = y1 + y2
+        else:
+            y = gaussian(x[i],pos,wid, amp)*nm
+
+        y_all = np.append(y_all, y)
+    return y_all
+
+def double_peak(x, pos1, wid1, r1, amp1, pos2, wid2, r2, amp2):
+    b1 = biGauss(x, pos1, wid1, r1, amp1)
+    b2 = biGauss(x, pos2, wid2, r2, amp2)
+    b = np.append(b1, b2)
+    return b1+b2
+
+def log_likelihood_biGauss(theta, n, x):
+    pos, wid, r, amp = theta
+    model = biGauss(x, pos, wid, r, amp)
+    L = model - (n*np.log(model))
+    #print('*****************One Peak***************')
+    #print(theta, np.sum(L))
+    return np.sum(L)
+
+def log_likelihood_doublePeak(theta, n, x):
+    pos1, wid1, r1, amp1, pos2, wid2, r2, amp2 = theta
+    model = double_peak(x, pos1, wid1, r1, amp1, pos2, wid2, r2, amp2)
+    L = model - (n*np.log(model))
+    #print('*****************Double Peak***************')
+    #print(theta, np.sum(L))
+    return np.sum(L)
+
+def likelihood_ratio_doublePeak(x, n, pos1, wid1, r1, amp1, pos2, wid2, r2, amp2):
+    model = double_peak(x, pos1, wid1, r1, amp1, pos2, wid2, r2, amp2)
+    val = model - n + (n*np.log(n/model))
+    #print('log - ', n/model, 'n - ', n)
+    return np.sum(val)
+
+def likelihood_ratio_biGauss(x, n, pos, wid, r, amp):
+    model = biGauss(x, pos, wid, r, amp)
+    val = model - n + (n*np.log(n/model))
+    #print('log - ', n/model, 'n - ', n)
+    return np.sum(val)
